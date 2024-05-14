4a2a04bf2634917e2e2a7b0b9f313ac6bcbdec52
govindmahto <thespyrant@gmail.com>
2024-05-14 17:57:47 +0530

    Update prime.js

--- prime.js
+++ prime.js
@@ -2,10 +2,13 @@ function returnNumber(){
     let a = document.getElementById("inputNumber").value;
 let b;
 let c;
-if(a==2){
-    c = "is a prime number"
+if(a<=1){
+    c = "is a neither prime nor composite"
 }
-for(b=2; b<a; b++){
+if (a==2||a==3){
+    c = " is a prime number"
+}
+for(b=2; b*b<=a; b++){
     if(a%b==0){
        c = "is not a prime number";
 
